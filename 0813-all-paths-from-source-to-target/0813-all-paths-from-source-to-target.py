0a659180a2ceb113c40325aa44cde88645b2a667
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-08 19:11:48 +1100

    Time: 4 ms (66.95%), Space: 18.8 MB (33.77%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = []
+        stack = [(0, [0])]
+
+        while stack:
+            node, path = stack.pop()
+
+            if node == len(graph) - 1:
+                paths.append(path)
+
+            for neighbor in graph[node]:
+                stack.append((neighbor, path + [neighbor]))
+
+        return paths
