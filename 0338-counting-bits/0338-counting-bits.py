2ef853083c76acbbf2d5da77c38e374beaed78a4
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-20 01:06:37 +1100

    Time: 7 ms (52.27%), Space: 18.6 MB (37.32%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -6,13 +6,16 @@ class Solution:
         # 1 2 4 8 16 32 64 128
         # 1 2 3 4 5  6  7  8
         # 6 100 - 64
-        store = [0] * (n + 1)
-        sub = 1
+        # store = [0] * (n + 1)
+        # sub = 1
 
-        for i in range(1, n + 1):
-            if sub * 2 == i:
-                sub = i
-            store[i] = store[i - sub] + 1
+        # for i in range(1, n + 1):
+        #     if sub * 2 == i:
+        #         sub = i
+        #     store[i] = store[i - sub] + 1
 
-        return store
-
+        # return store
+        res = [0] * (n+1)
+        for i in range(1, n+1):
+            res[i] = (i & 1) + res[i >> 1]
+        return res
