a934de73b8425744910134c07f4e7aa19b8c7c9a
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-20 00:56:00 +1100

    Time: 3 ms (94.99%), Space: 18.4 MB (59.85%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,18 @@
+import math
+
+class Solution:
+
+    def countBits(self, n: int) -> List[int]:
+        # 1 2 4 8 16 32 64 128
+        # 1 2 3 4 5  6  7  8
+        # 6 100 - 64
+        store = [0] * (n + 1)
+        sub = 1
+
+        for i in range(1, n + 1):
+            if sub * 2 == i:
+                sub = i
+            store[i] = store[i - sub] + 1
+
+        return store
+
