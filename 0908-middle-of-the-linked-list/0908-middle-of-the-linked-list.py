65b4bddb7e4def7d0296cc7ec06eb480a305264e
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-20 22:56:18 +1100

    Time: 0 ms (100%), Space: 17.7 MB (46.79%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -5,18 +5,30 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        size = 0
-        current = head
-        while current:
-            current = current.next
-            size += 1
+        ## Two passes through linked list, i don't like
+        # size = 0
+        # current = head
+        # while current:
+        #     current = current.next
+        #     size += 1
 
-        mid = size // 2
-        # if size % 2 == 0:
-        #     mid += 1
+        # mid = size // 2
         
-        current = head
-        for i in range(mid):
-            current = current.next
+        # current = head
+        # for i in range(mid):
+        #     current = current.next
 
-        return current
+        # return current
+        
+        ## Single pass using slow, fast pointers
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        if fast.next:
+            fast = fast.next
+            slow = slow.next
+        
+        return slow
