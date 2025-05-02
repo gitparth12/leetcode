61e27f864858f2c89cf8a0ddf95e53851ac7df0e
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-05-02 11:47:17 +0530

    Time: 89 ms (5.44%), Space: 49.4 MB (5%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        return self.recursion(head)
+        
+    def make_list(self, head: Optional[ListNode]) -> bool:
         # can't think of O(n) time and O(1) space
         # O(n) time and O(1) space attempt
 
@@ -19,4 +22,15 @@ class Solution:
         if len(arr) % 2 == 0:
             return arr[:len(arr)//2] == list(reversed(arr[len(arr)//2:]))
         return arr[:len(arr)//2] == list(reversed(arr[len(arr)//2+1:]))
-        
+    
+
+    def recursion(self, head: Optional[ListNode]) -> bool:
+        self.curr = head
+        return self.solve(head)
+
+    def solve(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return True
+        ans = self.solve(head.next) and head.val == self.curr.val
+        self.curr = self.curr.next
+        return ans
