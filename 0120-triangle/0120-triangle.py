a5d438f15fc83206d4b6f5b5e892a99bcf9dc804
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2026-03-10 18:17:18 +1100

    Time: 15 ms (6.62%), Space: 23.2 MB (10.1%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = {}  # (i, j) = min sum path from triangle[i][j] to bottom
+        def dfs(i, j):
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            dp[(i, j)] = triangle[i][j] + min(dfs(i+1, j), dfs(i+1, j+1))
+            return dp[(i, j)]
+        return dfs(0, 0)
+
+# dp solution: at the top, we need to know which path leads to the minimum sum
+# for a top-down solution, dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+# bottom-up optimisation: keep track of only two rows because we never go past 1 row access
