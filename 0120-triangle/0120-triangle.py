b709963548f265ed5e6ea7b319302971852b142b
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2026-03-10 18:43:05 +1100

    Time: 3 ms (72.54%), Space: 20.2 MB (61.51%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,6 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        return bottom_up(triangle)
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = triangle[i][j] + min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
 
 def bottom_up(triangle: List[List[int]]) -> int:
     # dp = triangle[-1]  # Can do inplace by treating current row in triangle as dp
